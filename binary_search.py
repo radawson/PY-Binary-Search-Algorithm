fd2740687ec4c416a607b4cdfa60714dc8a7f452
Rick Dawson <dawsora@gmail.com>
2023-11-01 20:42:44 -0400

    recursive and non-recursive functions

--- binary_search.py
+++ binary_search.py
@@ -1,2 +1,42 @@
 # code your iterative algorithm here
+def binary_search(data, el):
+    first = 0
+    last = len(data) - 1
+    found = False
 
+    while first <= last and not found:
+        mid = (first + last) // 2
+        if data[mid] == el:
+            found = True
+        else:
+            if el < data[mid]:
+                last = mid - 1
+            else:
+                first = mid + 1
+    return found
+
+
+def recursive_bsearch(data, el):
+    if len(data) == 0:
+        return False
+
+    else:
+        mid = len(data) // 2
+
+        if data[mid] == el:
+            return True
+        else:
+            if el < data[mid]:
+                return recursive_bsearch(data[:mid], el)
+            else:
+                return recursive_bsearch(data[mid + 1 :], el)
+
+
+if __name__ == "__main__":
+    test_list = [5, 8, 12, 14, 19, 22, 27, 30, 31]
+
+    print(binary_search(test_list, 12))  # True
+    print(binary_search(test_list, 25))  # False
+    #print("_" * 40)
+    #print(recursive_bsearch(test_list, 12))  # True
+    #print(recursive_bsearch(test_list, 25))  # False
